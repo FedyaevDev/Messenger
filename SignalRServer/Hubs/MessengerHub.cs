7c842c77cc9e0678bdfd96f7ef7f5e382d7349ae
FedayevR <fedayevDev@yandex.ru>
2023-05-12 00:54:55 +0700

    написал логику отправки сообщений и присоединения к серверу через signalr

---
+++ SignalRServer/Hubs/MessengerHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Models.Models;
+using Models.ModelShells;
+using System.Threading.Tasks;
+
+namespace SignalRServer.Hubs
+{
+    public class MessengerHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+        public async Task SendUser(UserShellForServer user)
+        {
+            await Clients.All.SendAsync("ReceiveUser", user);
+        }
+    }
+}
